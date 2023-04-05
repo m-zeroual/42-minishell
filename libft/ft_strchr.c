d9bcf805e5c7cfcf8f48a0880b3908c1182d75bd
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-04-05 23:03:43 +0000

    fix some problems in libft

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 18:53:18 by esalim            #+#    #+#             */
-/*   Updated: 2022/10/09 14:10:41 by esalim           ###   ########.fr       */
+/*   Updated: 2023/04/05 22:52:57 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 char	*ft_strchr(const char *str, int c)
 {
 	int	i;
-	int	len;
 
-	i = 0;
-	len = ft_strlen(str);
-	while (i <= len)
-	{
+    if (!str || !*str)
+        return (0);
+    i = -1;
+	while (str[++i])
 		if (str[i] == (unsigned char)c)
 			return ((char *)str + i);
-		i++;
-	}
 	return (0);
 }
--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:17:10 by esalim            #+#    #+#             */
-/*   Updated: 2022/10/02 14:50:06 by esalim           ###   ########.fr       */
+/*   Updated: 2023/04/05 22:38:35 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 size_t	ft_strlen(const char *str)
 {
 	int	length;
-
+    if (!str || !*str)
+        return (0);
 	length = 0;
 	while (str[length])
 		length++;
